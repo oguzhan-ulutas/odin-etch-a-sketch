ffae08d7b8600576bc95dc0e2248ac9cc4a9c9bf
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-18 01:13:18 +0300

    Add rainbow effect to rainbow function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.querySelector(".container");
-console.log(container);
 
-// Creates 16 x 16 grids when page loaded.
+// Creates 16 x 16 grids when page loaded. And creates grid up to
+// 100 x 100 when asked.
 
 function gridCreator(gridSide = 16) {
     
     for (let i = 0; i < (gridSide * gridSide); i++) {
-        let cellSide = Math.round(500/gridSide - 3);
+        let cellSide = Math.round(500/gridSide - 3); //500 is length of the container div.
         const div = document.createElement("div");
         div.classList.add("cell");
         div.setAttribute("style", `width: ${cellSide}px; height: ${cellSide}px;`);
@@ -16,3 +16,17 @@ function gridCreator(gridSide = 16) {
 };
 
 window.onload = gridCreator();
+
+// Add rainbow effect to rainbow button.
+
+const rainbow = document.querySelector(".rainbow");
+rainbow.addEventListener("mousemove", rainbowEffect);
+
+function rainbowEffect (e) {
+    rainbow.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 35)";
+};
+
+// Add new grid button function
+
+
+
