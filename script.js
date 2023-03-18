1641ba129d0954f584a7da73eb30fda5b9ccffe6
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-18 07:23:12 +0300

    Everything works as expected

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function gridCreator(gridSide = 16) {
 
 window.onload = gridCreator();
 
+
 // Add rainbow effect to rainbow button.
 
 const rainbow = document.querySelector(".rainbow");
@@ -32,8 +33,6 @@ const newGrid = document.querySelector(".newGrid");
 newGrid.addEventListener("click", regenerateGrid)
 
 
-// console.log(cells);
-
 function regenerateGrid () {
     const cells = document.querySelectorAll(".cell");
     let gridSide = prompt("Please enter a number between 16 and 100:");
@@ -43,17 +42,73 @@ function regenerateGrid () {
     gridCreator(gridSide);
 };
 
-// Black button 
+// Clears grid each button press
+
+function resetGrid () {
+    const cells = document.querySelectorAll(".cell");
+    gridSide = Math.sqrt(cells.length);
+    cells.forEach(cell => container.removeChild(cell));
+    gridCreator(gridSide);
+}
+
+// black button 
 
 const black = document.querySelector(".black");
-// console.log(black);
-black.addEventListener("click",blackEtch);
+black.addEventListener("click", blackEtch);
 
 function blackEtch () {
+    resetGrid();
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
-        cell.addEventListener("mouseenter", () =>{
-            cell.style.backgroundColor = "black";
+        cell.addEventListener("mouseenter",() =>{
+            cell.style.backgroundColor = "rgb(0, 0, 0)";
         });
     });
 };
+
+// grayTo button 
+
+const grayTo = document.querySelector(".grayTo");
+grayTo.addEventListener("click", grayToBlack);
+
+function grayToBlack () {
+    resetGrid();
+    const cells = document.querySelectorAll(".cell");
+    
+
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = "rgb(255, 255, 255)";
+    });
+
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", (e) => {
+            let rgb = e.target.style.backgroundColor;
+            console.log(e.target.style.backgroundColor)
+            if (rgb.length == 18 ) {
+                rgb = e.target.style.backgroundColor.slice(4,7) - 25.5;
+            } else {
+                rgb = e.target.style.backgroundColor.slice(4,6) - 25.5;
+            }
+            cell.style.backgroundColor = `rgb(${rgb}, ${rgb}, ${rgb})`;
+        });
+    });
+};
+
+// Rainbow button
+
+rainbow.addEventListener("click", rainbowEtch);
+
+function rainbowEtch(){
+    resetGrid();
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        console.log(randomColor);
+        cell.addEventListener("mouseenter",() =>{
+            cell.style.backgroundColor = "#" + randomColor;
+        });
+    });
+
+};
+
+
